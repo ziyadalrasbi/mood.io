c1a78ae2b33de60efc0258c97180e8c11646aca7
ziyadalrasbi <ziyadr29@yahoo.com>
2021-12-07 21:46:36 +0000

    beginning home interface

--- App.js
+++ App.js
@@ -1,37 +1,138 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { Linking, StyleSheet, Text, View } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 import { registerRootComponent } from 'expo';
 import { Button } from 'react-native-paper';
+import { useFonts } from 'expo-font' 
 
-export default function App() {
+function App() {
+
+
+  const [loaded] = useFonts({
+    InconsolataBold: require('./assets/fonts/Inconsolata/static/Inconsolata/Inconsolata-Bold.ttf'),
+    InconsolataLight: require('./assets/fonts/Inconsolata/static/Inconsolata/Inconsolata-Light.ttf'),
+    InconsolataMedium: require('./assets/fonts/Inconsolata/static/Inconsolata/Inconsolata-Medium.ttf'),
+    InconsolataBlack: require('./assets/fonts/Inconsolata/static/Inconsolata/Inconsolata-Black.ttf')
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text
-        style = {{ color: 'blue', fontSize: 24}}
-      > 
-      mood.io
-      </Text>
-      <Button
+      <View style={topStyles.container}>
+        <Text style={topStyles.welcome}>
+        welcome, user! 
+        </Text> 
+        <Text style={topStyles.subWelcome}>
+        how are you feeling today? 
+        </Text> 
+      </View> 
+      <View style={topStyles.subTop}>
+      </View>
+      <View style={firstStyles.container}>
+        <Text style={firstStyles.header}>
+          let's find some new music!
+        </Text>
+        <Text style={firstStyles.subHeader}>
+          press the button below to scan your mood 😎
+        </Text>
+        <Button
+        style={firstStyles.startButton}
         uppercase={false}
         mode="contained"
-        color="#f08e25"
-        contentStyle={{ height: 44 }}
-        labelStyle={{ color: 'black', fontSize: 18 }}
+        labelStyle={styles.font }
       >
-        Hello
+        get started
       </Button>
-      <StatusBar style="auto" />
+      </View>
+      <StatusBar style="auto"/>
     </View>
   );
 }
+
+const topStyles = StyleSheet.create({
+  container: {
+    position: 'relative',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: '30%',
+    width: '100%',
+    justifyContent: 'center',
+    backgroundColor: '#463aaa'
+  },
+  subTop: {
+    position: 'relative',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: '2%',
+    width:'100%',
+    backgroundColor: '#6b5be0'
+  },
+  welcome: {
+    padding: 10,
+    fontFamily: 'InconsolataBold',
+    color: 'white',
+    fontSize: 50
+  },
+  subWelcome: {
+    padding: 10,
+    fontFamily: 'InconsolataLight',
+    color: 'white',
+    fontSize: 26
+  }
+})
+
+const firstStyles = StyleSheet.create({
+  container: {
+    position: 'relative',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: '20%',
+    width: '100%',
+    justifyContent: 'flex-start',
+    backgroundColor: 'white'
+  },
+  header: {
+    marginTop: 5,
+    fontFamily: 'InconsolataBlack',
+    fontSize: 22,
+    padding: 10,
+    marginBottom: -10
+  },
+  subHeader: {
+    fontFamily: 'InconsolataMedium',
+    fontSize: 18,
+    padding: 10,
+    color: 'grey'
+  },
+  startButton: {
+    backgroundColor: '#4494da',
+    marginLeft: 8,
+    marginTop: 15,
+    padding: 5,
+    alignSelf: 'flex-start'
+  }
+})
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center',
+    display: 'flex',
+    justifyContent: 'flex-start'
   },
+  font: {
+    fontFamily: 'InconsolataMedium',
+    fontSize: 22
+  }
 });
+
+export default App;
