979045c4716401b6a013fe4dc15dc4d241b4f56a
ziyadalrasbi <ziyadr29@yahoo.com>
2021-12-07 15:27:31 +0000

    first testing on ios

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { registerRootComponent } from 'expo';
+import { Button } from 'react-native-paper';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Testing</Text>
+      <Text
+        style = {{ color: 'blue', fontSize: 24}}
+      > 
+      mood.io
+      </Text>
+      <Button
+        uppercase={false}
+        mode="contained"
+        color="#f08e25"
+        contentStyle={{ height: 44 }}
+        labelStyle={{ color: 'black', fontSize: 18 }}
+      >
+        Hello
+      </Button>
       <StatusBar style="auto" />
     </View>
   );
