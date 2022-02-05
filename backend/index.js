6fe58cff7191612e2f4a963a8f9a2a153bedf6a6
ziyadalrasbi <ziyadr29@yahoo.com>
2022-02-05 14:07:14 +0000

    testing redirect uri

--- backend/index.js
+++ backend/index.js
@@ -49,7 +49,7 @@ app.get('/callback', function (req, res) {
             url: 'https://accounts.spotify.com/api/token',
             form: {
                 code: code,
-                redirect_uri: redirect_uri,
+                redirect_uri: 'https://mood-io-app.herokuapp.com/callback',
                 grant_type: 'authorization_code'
             },
             headers: {
