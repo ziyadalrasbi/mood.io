e019572b3ffee6440d260df57f845b764bc9e30c
ziyadalrasbi <ziyadr29@yahoo.com>
2022-02-06 17:50:03 +0000

    endpoint created for getting recommendations

--- backend/database/controllers/homeController.js
+++ backend/database/controllers/homeController.js
@@ -6,15 +6,16 @@ const firebase = require('../db.js');
 const getRecommendations = async (req, res, next) => {
     try {
         var recommendations = [];
-        const response = firebase.firestore().collection('users').doc(JSON.stringify(req.body.user)).collection('recommendations').get();
-        response.forEach(doc => {
-            let recommendation = {
-                mood: doc.data().mood, 
-                time: doc.data().time, 
-                tracks: JSON.parse(doc.data().tracks)
-            }
-            recommendations.push(recommendation);
-        })
+        firebase.firestore()
+            .collection('users')
+            .doc(JSON.stringify(req.body.user))
+            .collection('recommendations')
+            .orderBy('time', 'desc')
+            .onSnapshot((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    recommendations.push(...doc.data());
+                })
+            })
         res.json({ recommendations: recommendations });
     } catch (error) {
         console.log('There was an error getting recommendations, please try again. ' + error);
